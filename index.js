721e633a930dd506f23d1c5c754ef638c8c3972b
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-08-18 11:47:48 -0700

    Added function to create tables to index.js

--- index.js
+++ index.js
@@ -5,7 +5,31 @@ client.on("error", (error) => {
     console.error(error.stack())
 })
 
+const createTables = async () => {
+    await client.query(`
+        CREATE TABLE IF NOT EXISTS costumes(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(80) NOT NULL,
+            category VARCHAR NOT NULL CHECK (
+                category='baby' or 
+                category='child' or 
+                category='adult' or 
+                category='pet'
+            ),
+            gender VARCHAR NOT NULL CHECK (
+                category='male' or 
+                category='female' or 
+                category='unisex'
+            ),
+            size VARCHAR(10) NOT NULL,
+            type VARCHAR(80) NOT NULL,
+            stock_count NUMBER NOT NULL,
+            price NUMBER NOT NULL 
+        );
+    `)
+}
 
 module.exports = {
-    client
+    client,
+    createTables
 }
