76c7a1e376d3d497a0832bd865987630949d66cb
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-08-21 11:57:45 -0700

    Added test to createTables.test.js

--- __tests__/createTables.test.js
+++ __tests__/createTables.test.js
@@ -1,18 +1,25 @@
 const {client, createTables} = require(".././index");
 
 describe("createTables adapter", () => {
-    beforeAll( async () => {
+    // Connect to postgres database before each test
+    beforeAll(async () => {
         await client.connect();
         console.log("connected");
     })
-    afterAll( async () => {
+    // Disconnect from postgres database after each test
+    afterAll(async () => {
         await client.end();
         console.log("connection closed");
     })
 
-    it("should create table names 'costumes'", async () => {
+    it("should create a table", async () => {
         const table = await createTables();
-        console.log("successfully created tables");
-        expect(table).toBe("cos")
+        expect(table).toBeTruthy();
+
+    })
+
+    it("should create table with the name 'costumes'", async () => {
+        const table = await createTables();
+        expect(table).toBe("costumes")
     })
 })
