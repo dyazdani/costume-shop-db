eb975649b5ce978190f7356c9ceb250dc4fe8a83
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-08-22 06:45:19 -0700

    Added test to updateCostume.test.js

--- __tests__/updateCostume.test.js
+++ __tests__/updateCostume.test.js
@@ -51,4 +51,47 @@ describe("updateCostume adapter", () => {
         expect(shortsAgain.name).toBe("very short shorts");
         client.release();
     })
+
+    it("should update costume values when all values are changed", async () => {
+        const client = await pool.connect();
+        console.log("connected");
+        await createTables();
+        await createCostume(
+            "chestplate",
+            "adult",
+            "unisex",
+            "M",
+            "armor",
+            1,
+            73.98
+        );
+        const chestplate = await getCostumeById(1);
+        expect(chestplate.name).toBe("chestplate");
+        expect(chestplate.category).toBe("adult");
+        expect(chestplate.gender).toBe("unisex");
+        expect(chestplate.size).toBe("M");
+        expect(chestplate.type).toBe("armor");
+        expect(chestplate.stock_count).toBe(1);
+        expect(chestplate.price).toBe(73.98);
+
+        await updateCostume(
+            1,
+            "breastplate",
+            "pet",
+            "female",
+            "S",
+            "dog",
+            3,
+            33.98
+        );
+        const breastplate = await getCostumeById(1);
+        expect(breastplate.name).toBe("breastplate");
+        expect(breastplate.category).toBe("pet");
+        expect(breastplate.gender).toBe("female");
+        expect(breastplate.size).toBe("S");
+        expect(breastplate.type).toBe("dog");
+        expect(breastplate.stock_count).toBe(3);
+        expect(breastplate.price).toBe(33.98);
+        client.release();
+    })
 })
