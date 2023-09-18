25546900a3c2c5ea28ee51f20c8053e9d4faab57
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-09-18 15:03:12 -0700

    Added test in getCostumeById.test.js for error message when id is non-existent

--- __tests__/endpoints/getCostumeById.test.js
+++ __tests__/endpoints/getCostumeById.test.js
@@ -27,9 +27,21 @@ describe('GET api/costumes/:id', () => {
         await createCostume(pool, getBallroomGown());
         await createCostume(pool, getButtlessChaps());
         await createCostume(pool, getBonnet());
-
         const response = await request.get('/api/costumes/2');
         expect(response.status).toBe(200);
         expect(response.body.name).toBe('buttless chaps');
       })
+
+      it("should respond with error message if given an ID that does not exist", async () => {
+        expect.hasAssertions();
+
+        await createTables(pool);
+        await createCostume(pool, getBallroomGown());
+        await createCostume(pool, getButtlessChaps());
+        await createCostume(pool, getBonnet());
+
+        const response = await request.get('/api/costumes/4');
+        expect(response.status).toBe(500);
+        expect(response.body.message).toBe('Failed to get costume with id 4');
+    })  
 })
