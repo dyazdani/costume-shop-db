90cf1712e04a24bdda775c8ebcbace8ae339d5b3
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-09-18 10:03:20 -0700

    Made PR review suggestions: reword describe and it labels, remove beforeEach, made comment to move DB test to another module

--- __tests__/endpoints/getCostumes.test.js
+++ __tests__/endpoints/getCostumes.test.js
@@ -15,26 +15,21 @@ const request = supertest(server)
 
 let pool = getPool();
 
-// Double-check that correct database is being used. 
-if (pool.options.database !== 'costume_shop_db_test') {
-    throw new Error("Pool instance was not assigned testing database. Testing aborted. Be sure that NODE_ENV environment variable is set to 'test'.")
-}
+// TODO: Put this DB check in another test. 
+// if (pool.options.database !== 'costume_shop_db_test') {
+//     throw new Error("Pool instance was not assigned testing database. Testing aborted. Be sure that NODE_ENV environment variable is set to 'test'.")
+// }
 
-beforeEach( async () => {
-    await createTables(pool);
-    await createCostume(pool, getBallroomGown());;
-})
-
-afterAll(async () => {
-    await pool.end()
-})
-
-
-describe('Testing api/costumes endpoint', () => {
-    it('gets the test endpoint', async () => {
-        const response = await request.get('/api/costumes')
-        console.log(response)
-        expect(response.status).toBe(200)
-        expect(response.body[0].name).toBe('ballroom gown')
+describe('GET api/costumes', () => {
+    afterAll(async () => {
+        await pool.end()
+    })
+    
+    it('returns all costumes', async () => {
+        await createTables(pool);
+        await createCostume(pool, getBallroomGown());
+        const response = await request.get('/api/costumes');
+        expect(response.status).toBe(200);
+        expect(response.body[0].name).toBe('ballroom gown');
       })
 })
