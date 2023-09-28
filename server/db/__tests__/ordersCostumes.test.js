2e8c1da81bf6f2642a0ddf8e41e7fde3a4aa3440
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-09-28 10:03:54 -0700

    fix typo

--- server/db/__tests__/ordersCostumes.test.js
+++ server/db/__tests__/ordersCostumes.test.js
@@ -40,7 +40,7 @@ if (pool.options.database !== 'costume_shop_db_test') {
     throw new Error("Pool instance was not assigned testing database. Testing aborted. Be sure that NODE_ENV environment variable is set to 'test'.")
 }
 
-describe('Orders Costumers DB', () => {
+describe('Orders Costumes DB', () => {
     beforeEach(async () => {
         await createTables(pool);
 
