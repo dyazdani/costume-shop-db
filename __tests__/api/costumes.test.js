e525c856b6a65610e8e2116fd9a8712dfc8e7cbb
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-02 11:19:28 -0700

    Removed hard-coded data from jest assertions in costumes.test.js

--- __tests__/api/costumes.test.js
+++ __tests__/api/costumes.test.js
@@ -25,41 +25,46 @@ describe('/api/costumes', () => {
     })
     describe('GET api/costumes', () => {
         it('should return all costumes when there is one in DB', async () => {
-            await createCostume(pool, getBallroomGown());
+            const gown = await createCostume(pool, getBallroomGown());
             const response = await request.get('/api/costumes');
             expect(response.status).toBe(200);
-            expect(response.body.costumes[0].name).toBe('ballroom gown');
+            expect(response.body.costumes[0].name).toBe(gown.name);
           })
     
           it('should return all costumes when there is multiple in DB', async () => {
-            await createCostume(pool, getBallroomGown());
-            await createCostume(pool, getButtlessChaps());
-            await createCostume(pool, getBonnet());
+            const gown = await createCostume(pool, getBallroomGown());
+            const chaps = await createCostume(pool, getButtlessChaps());
+            const bonnet = await createCostume(pool, getBonnet());
             const response = await request.get('/api/costumes');
             expect(response.status).toBe(200);
-            expect(response.body.costumes[0].name).toBe('ballroom gown');
-            expect(response.body.costumes[1].name).toBe('buttless chaps');
-            expect(response.body.costumes[2].name).toBe('bonnet');
+            expect(response.body.costumes[0].name).toBe(gown.name);
+            expect(response.body.costumes[1].name).toBe(chaps.name);
+            expect(response.body.costumes[2].name).toBe(bonnet.name);
           })
     })
     describe('POST /api/costumes', () => {
         it('should succeed in POST request', async () => {
-            const response = await request.post('/api/costumes').send(getBonnet());
+            const bonnet = getBonnet();
+            const response = await request.post('/api/costumes').send(bonnet);
             expect(response.status).toBe(200);
-            expect(response.body.costume.name).toBe('bonnet');
+            expect(response.body.costume.name).toBe(bonnet.name);
         })
         it('should succeed with consecutive POST requests', async () => {
-            const responseOne = await request.post('/api/costumes').send(getBonnet());
-            const responseTwo = await request.post('/api/costumes').send(getButtlessChaps());
-            const responseThree = await request.post('/api/costumes').send(getBallroomGown());
+            const bonnet = getBonnet();
+            const chaps = getButtlessChaps();
+            const gown = getBallroomGown();
+            
+            const responseOne = await request.post('/api/costumes').send(bonnet);
+            const responseTwo = await request.post('/api/costumes').send(chaps);
+            const responseThree = await request.post('/api/costumes').send(gown);
            
             expect(responseOne.status).toBe(200);
             expect(responseTwo.status).toBe(200);
             expect(responseThree.status).toBe(200);
 
-            expect(responseOne.body.costume.name).toBe('bonnet');
-            expect(responseTwo.body.costume.name).toBe('buttless chaps');
-            expect(responseThree.body.costume.name).toBe('ballroom gown');
+            expect(responseOne.body.costume.name).toBe(bonnet.name);
+            expect(responseTwo.body.costume.name).toBe(chaps.name);
+            expect(responseThree.body.costume.name).toBe(gown.name);
         })
     })
 })
