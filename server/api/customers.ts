16f3503513f255c5ed6eb2ae1f702d389dffc7a4
Josh van Eyken <josh.vaneyken@gmail.com>
2023-10-03 13:53:11 -0700

    Add GET /api/customers/:id and associated tests, small fix for GET /api/customers test

--- server/api/customers.ts
+++ server/api/customers.ts
@@ -1,16 +1,28 @@
 import express from "express";
 
-const { getAllCustomers, getPool } = require("../db");
+const { getAllCustomers, getCustomerById, getPool } = require("../db");
 
 const pool = getPool();
 
 const customerRouter = express.Router();
 
 // GET /api/customers
-customerRouter.get("/",async (req, res, next): Promise<void> => {
+customerRouter.get("/", async (req, res, next): Promise<void> => {
     try {
         const customers = await getAllCustomers(pool);
         res.send({customers});
+    } catch(error) {
+        next(error);
+    }
+})
+
+// GET /api/customers/:id
+customerRouter.get("/:id", async (req, res, next): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const customer = await getCustomerById(pool, id);
+        const { full_name, email } = customer;
+        res.send( {customer: { id, full_name, email }} );
     } catch(error) {
         next(error);
     }
