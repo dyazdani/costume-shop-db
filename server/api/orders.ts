5ca5d3a04ac6cc05265e4b57e031eef834f6e0f7
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-04 12:34:25 -0700

    Added endpoint for POST /api/orders

--- server/api/orders.ts
+++ server/api/orders.ts
@@ -8,12 +8,23 @@ const ordersRouter = express.Router();
 
 // GET /api/orders
 
-ordersRouter.get("/", async (req, res, next) => {
+ordersRouter.get("/", async (req, res, next): Promise<void> => {
     try {
         const orders = await getAllOrders(pool);
         res.send({orders});
     } catch (e) {
         next(e)
+    }
+})
+
+// POST /api/orders
+
+ordersRouter.post("/", async (req, res, next): Promise<void> => {
+    try {
+        const order = await createOrder(pool, req.body);
+        res.send({order});
+    } catch (e) {
+        next(e);
     }
 })
 
