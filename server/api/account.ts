52d697c0f61c53d5331d9d50de8220a58574b574
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-06 11:29:09 -0700

    Added comparison of hashed password to input password in api/account/login endpoint

--- server/api/account.ts
+++ server/api/account.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const bcrypt = require('bcrypt');
 
-const { getPool, createCustomer} = require('../db');
+const { getPool, createCustomer, getCustomerById} = require('../db');
 
 const pool = getPool();
 
@@ -9,17 +9,35 @@ const saltRounds = 10;
 
 const accountRouter = express.Router()
 
-// POST /api/register
+// POST /api/account/register
 
-accountRouter.post('/', async (req, res, next): Promise<void> => {
+accountRouter.post('/register', async (req, res, next): Promise<void> => {
     try {
-        console.log(req.body)
         const {fullName, email, password} = req.body;
         bcrypt.hash(password, saltRounds, async function(err: Error | undefined, hash: string) {
             const customer = await createCustomer(pool, fullName, email, hash);
             delete customer.password;
             res.send({customer});
           });
+    } catch (e) {
+        next(e);
+    }
+})
+
+// POST /api/account/login
+
+accountRouter.post('/login', async (req, res, next): Promise<void> => {
+    try {
+        const customer = await getCustomerById(pool, req.body.id);
+        const isValid = await bcrypt.compare(req.body.password, customer.password);
+
+        if (isValid) {
+            console.log('Password valid!')
+        } else {
+            console.log('Password is invalid!')
+        }
+        delete customer.password;
+        res.send({customer})
     } catch (e) {
         next(e);
     }
